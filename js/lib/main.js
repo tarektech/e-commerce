063459df32e1af96ae38f3da2889a62fcab5d2fc
tarek <tarek_zein97@hotmail.com>
2023-06-08 00:17:03 +0300

    adding slider

--- js/lib/main.js
+++ js/lib/main.js
@@ -1,31 +1,30 @@
-$(function() {
-
-	if ( $('.owl-2').length > 0 ) {
-        $('.owl-2').owlCarousel({
-            center: false,
-            items: 1,
-            loop: true,
-            stagePadding: 0,
-            margin: 20,
-            smartSpeed: 1000,
-            autoplay: true,
-            nav: true,
-            dots: true,
-            pauseOnHover: false,
-            responsive:{
-                600:{
-                    margin: 20,
-                    nav: true,
-                  items: 2
-                },
-                1000:{
-                    margin: 20,
-                    stagePadding: 0,
-                    nav: true,
-                  items: 3
-                }
-            }
-        });            
-    }
-
-})
+$(function () {
+  if ($('.owl-2').length > 0) {
+    $('.owl-2').owlCarousel({
+      center: false,
+      items: 1,
+      loop: true,
+      stagePadding: 0,
+      margin: 0,
+      smartSpeed: 1000,
+      autoplay: true,
+      nav: true,
+      dots: false,
+      pauseOnHover: false,
+      autoWidth: true,
+      responsive: {
+        600: {
+          margin: 0,
+          nav: true,
+          items: 2,
+        },
+        1000: {
+          margin: 0,
+          stagePadding: 0,
+          nav: true,
+          items: 3,
+        },
+      },
+    });
+  }
+});
